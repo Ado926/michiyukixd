04fa14af2652d07aa269bb9a021f316914620552
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-05-17 11:11:06 -0700

    Actualizar ai-gemini.js

--- plugins/ai-gemini.js
+++ plugins/ai-gemini.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `${emoji} Ingrese una petición para que Gemini lo responda.`, m)
+if (!text) return conn.reply(m.chat, `${emoji} іᥒgrᥱsᥲ ᥙᥒᥲ ⍴ᥱ𝗍іᥴі᥆́ᥒ ⍴ᥲrᥲ 𝗊ᥙᥱ ᥣᥱ ⍴ᥙᥱძᥲ rᥱs⍴᥆ᥒძᥱr.`, m)
 try {
 await m.react(rwait)
 conn.sendPresenceUpdate('composing', m.chat)
@@ -9,7 +9,7 @@ var res = await apii.json()
 await m.reply(res.result)
 } catch {
 await m.react('❌')
-await conn.reply(m.chat, `${msm} Gemini no puede responder a esa pregunta.`, m)
+await conn.reply(m.chat, `${msm} gᥱmіᥒі ᥒ᥆ ⍴ᥙᥱძᥱ rᥱs⍴᥆ᥒძᥱr ᥱs𝗍ᥲ ⍴rᥱgᥙᥒ𝗍ᥲ.`, m)
 }}
 handler.command = ['gemini']
 handler.help = ['gemini']
