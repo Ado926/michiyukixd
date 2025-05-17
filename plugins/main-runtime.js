7eddb6293ee489a93d6082e0f4e5395f11e9c7b3
Ado <adonaylester85@gmail.com>
2025-05-17 12:47:23 -0600

    Actualizar main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -2,7 +2,7 @@ let handler = async (m, { usedPrefix, command }) => {
 let uptime = await process.uptime()
 let runtime = `${packname}
 
-✰ Tiempo activo: ${rTime(uptime)}`
+🍄 Tiempo activo: ${rTime(uptime)}`
 conn.reply(m.chat, runtime, m)
 }
 handler.help = ['runtime']
