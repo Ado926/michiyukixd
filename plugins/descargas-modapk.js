182e65f18337598261dbf00cd4d8bcc80f8e1ac8
Ado <adonaylester85@gmail.com>
2025-05-17 12:22:57 -0600

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -7,11 +7,11 @@ await m.react(rwait)
 conn.reply(m.chat, `${emoji} Descargando su aplicación, espere un momento...`, m)
 let searchA = await search(text)
 let data5 = await download(searchA[0].id)
-let txt = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
-txt += `☁️ *Nombre* : ${data5.name}\n`
-txt += `🔖 *Package* : ${data5.package}\n`
-txt += `🚩 *Update* : ${data5.lastup}\n`
-txt += `⚖ *Peso* :  ${data5.size}`
+let txt = `✦･ﾟ:* *APTOIDE - Información* *:･ﾟ✦\n\n`
+txt += `🌴 Nombre: ${data5.name}\n`
+txt += `📦 Package: ${data5.package}\n`
+txt += `🕰 Última actualización: ${data5.lastup}\n`
+txt += `📏 Tamaño: ${data5.size}`
 await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m) 
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
