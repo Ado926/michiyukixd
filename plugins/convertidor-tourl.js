54e55385816540e7a8946b2cb8e00fd0a5b1aa41
Ado <adonaylester85@gmail.com>
2025-05-17 12:16:30 -0600

    Actualizar convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -12,12 +12,12 @@ let handler = async (m) => {
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   let img = await (await fetch(`${link}`)).buffer()
-  let txt = `乂  *L I N K - E N L A C E*  乂\n\n`
-      txt += `*» Enlace* : ${link}\n`
-      txt += `*» Acortado* : ${await shortUrl(link)}\n`
-      txt += `*» Tamaño* : ${formatBytes(media.length)}\n`
-      txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
-      txt += `> *${dev}*`
+  let txt = `📎 *L I N K  D E S C A R G A* 📎\n\n`
+txt += `🔗 *Enlace:* ${link}\n`
+txt += `🧩 *Acortado:* ${await shortUrl(link)}\n`
+txt += `📦 *Tamaño:* ${formatBytes(media.length)}\n`
+txt += `⏳ *Expira:* ${isTele ? 'No expira' : 'Desconocido'}\n\n`
+txt += `👤 *Generado por:* ${botname}`
 
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fkontak)
 await m.react(done)
