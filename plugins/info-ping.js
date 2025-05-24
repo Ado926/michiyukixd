6dea09061c3b8f7865138861777f147cee2361d8
Ado <adonaylester85@gmail.com>
2025-05-24 14:03:13 -0600

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,14 +1,20 @@
 import speed from 'performance-now'
-import { spawn, exec, execSync } from 'child_process'
+import { exec } from 'child_process'
 
 let handler = async (m, { conn }) => {
-         let timestamp = speed();
-         let latensi = speed() - timestamp;
-         exec(`neofetch --stdout`, (error, stdout, stderr) => {
-          let child = stdout.toString("utf-8");
-          let ssd = child.replace(/Memory:/, "Ram:");
+  let timestamp = speed()
+  let latensi = speed() - timestamp
+  exec(`neofetch --stdout`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(error)
+      return conn.reply(m.chat, 'Error ejecutando neofetch', m, global.bcanal)
+    }
+    let child = stdout.toString('utf-8')
+    let ssd = child.replace(/Memory:/, 'Ram:')
 
-          conn.reply(m.chat, `✰ *¡Pong!*\n> Tiempo ⴵ ${latensi.toFixed(4)}ms`, m, global.bcanal);
+    conn.reply(m.chat, `✰ *¡Pong!*\n> Tiempo ⴵ ${latensi.toFixed(4)}ms\n\n${ssd}`, m, global.bcanal)
+  })
+}
 
 handler.help = ['ping']
 handler.tags = ['info']
