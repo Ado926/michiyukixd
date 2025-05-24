7e7ef1dde8bbb7f0fb8ae93714f89ba8093afa21
Ado <adonaylester85@gmail.com>
2025-05-24 12:15:01 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -109,8 +109,6 @@ global.icono = [
 'https://qu.ax/HKtQj.jpg',
 ].getRandom()
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
-
 export default handler
 
 function pickRandom(list) {
