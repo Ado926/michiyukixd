23c690a3fe4a5bb7752c83192e77ffdeb5792dc4
Ado <adonaylester85@gmail.com>
2025-05-24 14:51:54 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -61,10 +61,10 @@ global.emoji4 = '୨୧'
 global.emoji5 = '♡'
 global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
 
-global.wait = '❍ Espera un momento, soy lenta...';
-global.waitt = '❍ Espera un momento, soy lenta...';
-global.waittt = '❍ Espera un momento, soy lenta...';
-global.waitttt = '❍ Espera un momento, soy lenta...';
+global.wait = ' Espera un momento, soy lento...';
+global.waitt = ' Espera un momento, soy lento...';
+global.waittt = ' Espera un momento, soy lento...';
+global.waitttt = ' Espera un momento, soy lento...';
 
 var canal = 'https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3w'  
 var comunidad = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
@@ -90,15 +90,19 @@ global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
-global.packsticker = `┊ ૮₍｡•́︿•̀｡₎ა *Mai Stickers*  
-╰┈➤ ୨୧ https://whatsapp.com/channel/0029Vb5UfTC4CrfeKSamhp1f ✿
-┊ ℹ️ *Info:*  
-╰┈➤ 𖥻 Mai By Wirk 🪴`;
+global.packsticker = `╭─〔 ʚ♡ɞ Michi Ai Stickers 〕─✧  
+│ ୨୧ Canal oficial:  
+│ https://whatsapp.com/channel/0029Vb5UfTC4CrfeKSamhp1f  
+│  
+│ 𖥸 Info:  
+│ By Wirk  
+╰─────────────♡`;
 
-global.packsticker2 = `\n┊🌸 Bot: ${botname}
-┊🌷 Usuario: ${nombre}
-┊🕰️ Fecha: ${fecha}
-╰┈➤ Hora: ${tiempo} ⏰`;
+global.packsticker2 = `\n╭─〔 🐱 Datos del Sticker 〕─✧  
+│ ✿ Bot: ${botname}  
+│ ✿ Usuario: ${nombre}  
+│ ✿ Fecha: ${fecha}  
+╰┈➤ 🕰️ Hora exacta: ${tiempo}`;
 
 global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
 
