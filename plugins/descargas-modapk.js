2a0955f887e43d52d8b79a699b2d9591e9ef7f61
Ado <adonaylester85@gmail.com>
2025-05-25 08:48:26 -0600

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -12,7 +12,7 @@ txt += `🌴 Nombre: ${data5.name}\n`
 txt += `📦 Package: ${data5.package}\n`
 txt += `🕰 Última actualización: ${data5.lastup}\n`
 txt += `📏 Tamaño: ${data5.size}`
-await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m) 
+await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, bcanal) 
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
 return await conn.reply(m.chat, `${emoji2} El archivo es demaciado pesado.`, m)}
@@ -25,6 +25,6 @@ handler.help = ['apkmod']
 handler.command = ['apk', 'modapk', 'aptoide']
 handler.group = true;
 handler.register = true;
-handler.coin = 5;
+handler.coin = 21;
 
 export default handler
