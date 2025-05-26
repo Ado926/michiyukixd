fcb1adfec22ebdb5f28cace973faa70ee0ae9c5c
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-05-25 21:40:01 -0700

    Actualizar convertidor-tomp3.js

--- plugins/convertidor-tomp3.js
+++ plugins/convertidor-tomp3.js
@@ -5,17 +5,17 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   const mime = (q || q.msg).mimetype || q.mediaType || '';
   
   if (!/video|audio/.test(mime)) {
-    return conn.reply(m.chat, `${emoji} Por favor, responda al video o nota de voz que desee convertir a Audio/MP3.`, m);
+    return conn.reply(m.chat, `${emoji} ⍴᥆r 𝖿ᥲ᥎᥆r, rᥱs⍴᥆ᥒძᥲ ᥲᥣ ᥎іძᥱ᥆ ᥆ ᥒ᥆𝗍ᥲ ძᥱ ᥎᥆z 𝗊ᥙᥱ ძᥱsᥱᥱ ᥴ᥆ᥒ᥎ᥱr𝗍іr ᥲ ᥲᥙძі᥆/m⍴3.`, m);
   }
   
   const media = await q.download();
   if (!media) {
-    return conn.reply(m.chat, `${msm} Ocurrio un error al descargar su video.`, m);
+    return conn.reply(m.chat, `${msm} ᥆ᥴᥙrrі᥆ ᥙᥒ ᥱrr᥆r.`, m);
   }
   
   const audio = await toAudio(media, 'mp4');
   if (!audio.data) {
-    return conn.reply(m.chat, `${msm} Ocurrio un error al convertir su nota de voz a Audio/MP3.`, m);
+    return conn.reply(m.chat, `${msm} ᥆ᥴᥙrrі᥆ ᥙᥒ ᥱrr᥆r.`, m);
   }
   
   conn.sendMessage(m.chat, {audio: audio.data, mimetype: 'audio/mpeg'}, {quoted: m});
