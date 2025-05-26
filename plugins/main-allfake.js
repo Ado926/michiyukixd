87f083e4cc643f3472d0c77e1ede6ad090bd7f16
Ado <adonaylester85@gmail.com>
2025-05-25 18:36:09 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -6,139 +6,134 @@ import moment from 'moment-timezone'
 const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 
 var handler = m => m
-handler.all = async function (m) {
 
-global.getBuffer = async function getBuffer(url, options) {
-try {
-options ? options : {}
-var res = await axios({
-method: "get",
-url,
-headers: {
-'DNT': 1,
-'User-Agent': 'GoogleBot',
-'Upgrade-Insecure-Request': 1
-},
-...options,
-responseType: 'arraybuffer'
-})
-return res.data
-} catch (e) {
-console.log(`Error : ${e}`)
-}}
+handler.all = async function (m, { conn }) {
+  // Función getBuffer
+  global.getBuffer = async function (url, options = {}) {
+    try {
+      const res = await axios({
+        method: "get",
+        url,
+        headers: {
+          'DNT': 1,
+          'User-Agent': 'GoogleBot',
+          'Upgrade-Insecure-Request': 1
+        },
+        ...options,
+        responseType: 'arraybuffer'
+      })
+      return res.data
+    } catch (e) {
+      console.log(`Error : ${e}`)
+      return null
+    }
+  }
 
-global.creador = 'Wa.me/393715279301'
-global.ofcbot = `${conn.user.jid.split('@')[0]}`
-global.asistencia = 'Wa.me/393715279301'
-global.namechannel = '=͟͟͞❀ sᥙmі - sᥲkᥙrᥲsᥲᥕᥲ  ⏤͟͟͞͞★'
-global.namechannel2 = '=͟͟͞❀ sᥙmі - sᥲkᥙrᥲsᥲᥕᥲ ⏤͟͟͞͞★'
-global.namegrupo = 'ᰔᩚ ᥡᥙkі sᥙ᥆ᥙ • ᥆𝖿іᥴіᥲᥣ ❀'
-global.namecomu = 'ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥴ᥆mᥙᥒі𝗍ᥡ ❀'
-global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
-global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
+  // Datos generales
+  global.creador = 'Wa.me/393715279301'
+  global.ofcbot = conn?.user?.jid ? conn.user.jid.split('@')[0] : 'BOT'
+  global.asistencia = 'Wa.me/393715279301'
+  global.namechannel = '=͟͟͞❀ sᥙmі - sᥲkᥙrᥲsᥲᥕᥲ  ⏤͟͟͞͞★'
+  global.namechannel2 = '=͟͟͞❀ sᥙmі - sᥲkᥙrᥲsᥲᥕᥲ ⏤͟͟͞͞★'
+  global.namegrupo = 'ᰔᩚ ᥡᥙkі sᥙ᥆ᥙ • ᥆𝖿іᥴіᥲᥣ ❀'
+  global.namecomu = 'ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥴ᥆mᥙᥒі𝗍ᥡ ❀'
+  global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
+  global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
 
-global.canalIdM = ["120363402846939411@newsletter", "120363402846939411@newsletter"]
-global.canalNombreM = [
-  "⪩ ᐉ 𝙑𝙞𝙫𝙤𝙨 𝙑𝙞𝙫𝙞𝙚𝙣𝙩𝙚𝙨 ⪨ ⚡",
-  "✺ 𝗏𝗂𝗏𝗈𝗌 𝗏𝗂𝗏𝗂𝖾𝗇𝗍𝖾𝗌 ☼",
-  "𓆩 Vɪᴠᴏs Vɪᴠɪᴇɴᴛᴇs 𓆪 ✦",
-  "⌗ 𝖵𝗂𝗏𝗈𝗌・𝖵𝗂𝗏𝗂𝖾𝗇𝗍𝖾𝗌 ⌬",
-  "✦ VΙVOS VIVΙENTES ™ 🌿"
-]
-global.channelRD = await getRandomChannel()
+  // Canal random
+  global.canalIdM = ["120363402846939411@newsletter", "120363402846939411@newsletter"]
+  global.canalNombreM = [
+    "⪩ ᐉ 𝙑𝙞𝙫𝙤𝙨 𝙑𝙞𝙫𝙞𝙚𝙣𝙩𝙚𝙨 ⪨ ⚡",
+    "✺ 𝗏𝗂𝗏𝗈𝗌 𝗏𝗂𝗏𝗂𝖾𝗇𝗍𝖾𝗌 ☼",
+    "𓆩 Vɪᴠᴏs Vɪᴠɪᴇɴᴛᴇs 𓆪 ✦",
+    "⌗ 𝖵𝗂𝗏𝗈𝗌・𝖵𝗂𝗏𝗂𝖾𝗇𝗍𝖾𝗌 ⌬",
+    "✦ VΙVOS VIVΙENTES ™ 🌿"
+  ]
+  global.channelRD = canalNombreM[Math.floor(Math.random() * canalNombreM.length)]
 
-global.d = new Date(new Date + 3600000)
-global.locale = 'es'
-global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
-global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
-global.mes = d.toLocaleDateString('es', {month: 'long'})
-global.año = d.toLocaleDateString('es', {year: 'numeric'})
-global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
+  // Tiempo
+  global.d = new Date(new Date + 3600000)
+  global.locale = 'es'
+  global.dia = d.toLocaleDateString(locale, { weekday: 'long' })
+  global.fecha = d.toLocaleDateString('es', { day: 'numeric', month: 'numeric', year: 'numeric' })
+  global.mes = d.toLocaleDateString('es', { month: 'long' })
+  global.año = d.toLocaleDateString('es', { year: 'numeric' })
+  global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
 
-global.rwait = '🕒'
-global.done = '✅'
-global.error = '✖️'
-global.msm = '⚠︎'
+  // Emojis y estados
+  global.rwait = '🕒'
+  global.done = '✅'
+  global.error = '✖️'
+  global.msm = '⚠︎'
+  global.emoji = '♡(｡- ω -)'
+  global.emoji2 = '(ฅ•.•ฅ)'
+  global.emoji3 = '꒰ ֊ ܸ. .ܸ ֊ ꒱'
+  global.emoji4 = '૮₍ ˶ᵔ ᵕ ᵔ˶ ₎ა'
+  global.emoji5 = '(｡・//ε//・｡)'
+  global.emojis = [emoji, emoji2, emoji3, emoji4][Math.floor(Math.random() * 4)]
 
-global.emoji = '♡(｡- ω -)'
-global.emoji2 = '(ฅ•.•ฅ)'
-global.emoji3 = '꒰ ֊ ܸ. .ܸ ֊ ꒱'
-global.emoji4 = '૮₍ ˶ᵔ ᵕ ᵔ˶ ₎ა'
-global.emoji5 = '(｡・//ε//・｡)'
-global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
+  global.wait = ' Espera un momento, soy lento...'
+  global.waitt = global.waittt = global.waitttt = global.wait
 
-global.wait = ' Espera un momento, soy lento...';
-global.waitt = ' Espera un momento, soy lento...';
-global.waittt = ' Espera un momento, soy lento...';
-global.waitttt = ' Espera un momento, soy lento...';
+  // Redes
+  var canal = 'https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3w'
+  var comunidad = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
+  var git = 'https://github.com/The-King-Destroy'
+  var github = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot'
+  let correo = 'thekingdestroy507@gmail.com'
+  global.redes = [canal, comunidad, git, github, correo][Math.floor(Math.random() * 5)]
 
-var canal = 'https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3w'  
-var comunidad = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
-var git = 'https://github.com/The-King-Destroy'
-var github = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot' 
-let correo = 'thekingdestroy507@gmail.com'
-global.redes = [canal, comunidad, git, github, correo].getRandom()
+  // Icono aleatorio desde db
+  const category = "imagen"
+  const db = './src/database/db.json'
+  const db_ = JSON.parse(fs.readFileSync(db))
+  const randomlink = db_.links[category][Math.floor(Math.random() * db_.links[category].length)]
+  const response = await fetch(randomlink)
+  const rimg = await response.buffer()
+  global.icons = rimg
 
-let category = "imagen"
-const db = './src/database/db.json'
-const db_ = JSON.parse(fs.readFileSync(db))
-const random = Math.floor(Math.random() * db_.links[category].length)
-const randomlink = db_.links[category][random]
-const response = await fetch(randomlink)
-const rimg = await response.buffer()
-global.icons = rimg
+  // Saludo por hora
+  let h = new Date().getHours()
+  if (h < 3) global.saludo = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'
+  else if (h < 7) global.saludo = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'
+  else if (h < 12) global.saludo = 'Lɪɴᴅᴏ Dɪᴀ 🌤'
+  else if (h < 18) global.saludo = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'
+  else global.saludo = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'
 
-var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 1: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 3: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 4: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 5: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 6: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break; case 8: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 10: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 11: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 12: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 14: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 15: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 16: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 18: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 19: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 20: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 21: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 22: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 23: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;}
-global.saludo = hour;
+  // Nombre y formato
+  global.nombre = m.pushName || 'Anónimo'
+  global.taguser = '@' + m.sender.split("@")[0]
+  var more = String.fromCharCode(8206)
+  global.readMore = more.repeat(850)
 
-global.nombre = m.pushName || 'Anónimo'
-global.taguser = '@' + m.sender.split("@s.whatsapp.net")
-var more = String.fromCharCode(8206)
-global.readMore = more.repeat(850)
-
-global.packsticker = `╭─〔 ʚ♡ɞ Michi Ai Stickers 〕─✧  
+  // Stickers
+  global.packsticker = `╭─〔 ʚ♡ɞ Michi Ai Stickers 〕─✧  
 │ ୨୧ Canal oficial:  
 │ https://whatsapp.com/channel/0029Vb5UfTC4CrfeKSamhp1f  
 │  
 │ 𖥸 Info:  
 │ By Wirk  
-╰─────────────♡`;
+╰─────────────♡`
 
-global.packsticker2 = `\n╭─〔 🐱 Datos del Sticker 〕─✧  
-│ ✿ Bot: ${botname}  
+  global.packsticker2 = `\n╭─〔 🐱 Datos del Sticker 〕─✧  
+│ ✿ Bot: ${conn?.user?.name || 'Bot'}  
 │ ✿ Usuario: ${nombre}  
 │ ✿ Fecha: ${fecha}  
-╰┈➤ 🕰️ Hora exacta: ${tiempo}`;
+╰┈➤ 🕰️ Hora exacta: ${tiempo}`
 
-global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
-
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1 }
-}}, { quoted: m }
-
-global.icono = [
-'https://tinyurl.com/285a5ejf',
-].getRandom()
-
-global.rcanal = {
-  contextInfo: {
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: channelRD.id,
-      serverMessageId: 100,
-      newsletterName: channelRD.name
+  // Contacto falso
+  global.fkontak = {
+    key: {
+      participant: `0@s.whatsapp.net`,
+      ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {})
+    },
+    message: {
+      contactMessage: {
+        displayName: `${nombre}`,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre};;;\nFN:${nombre}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Celular\nEND:VCARD`
+      }
     }
   }
-};
-  
-export default handler
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]
-  }
-
-async function getRandomChannel() {
-let randomIndex = Math.floor(Math.random() * canalIdM.length)
-let id = canalIdM[randomIndex]
-let name = canalNombreM[randomIndex]
-return { id, name }
 }
+
+export default handlerq
